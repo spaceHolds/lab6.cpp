31b6692b5037afe6d39e07b06656be7e69667e35
Ivan <ivancernev19@gmail.com>
2023-10-16 21:11:15 +0300

    first commit

---
+++ lab6.cpp
@@ -0,0 +1,23 @@
+﻿#include "iostream"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Enter a natural number: ";
+    cin >> N;
+    string numstr = to_string(N);
+    size_t length = numstr.length() - 1;
+    string num2;
+    int index = 1;
+    num2 += numstr[length];
+    while (index < length) {
+        num2 += numstr[index];
+        index++;
+    }
+    num2 += numstr[0];
+    cout << num2 << endl;
+
+}
